ba1753f8d7016a08f3acec4e44a578516a82b42d
gusal <gus.alves.araujo@gmail.com>
2021-09-27 21:28:49 -0300

    Include and implement componente CampoInteiro

--- src/telas/Servicos/Item/index.js
+++ src/telas/Servicos/Item/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Text, TextInput, View } from 'react-native';
 
 import estilos from './estilos';
 
 export default function Item({nome, preco, descricao}) {
+    const [quantidade, setQuantidade] = useState(1);
+
     return <> 
         <View style={estilos.informacao}>
             <Text style={estilos.nome}>{ nome } </Text> 
@@ -14,7 +16,7 @@ export default function Item({nome, preco, descricao}) {
             <View>
                 <View style={estilos.valor}>
                     <Text style={estilos.descricao}>Quantidade:</Text>
-                    <TextInput value='0' />
+                    <TextInput valor={quantidade} acao={setQuantidade} />
                 </View>
                 <View style={estilos.valor}>
                     <Text style={estilos.descricao}>Preço:</Text>
