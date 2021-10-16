264f79e939ba76d75c0a9d21fbedff44ed88928d
gusal <gus.alves.araujo@gmail.com>
2021-10-16 08:20:36 -0300

    Update project

--- src/componentes/Botao/index.js
+++ src/componentes/Botao/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import { Text, TouchableOpacity } from 'react-native';
+import { TouchableOpacity, Text } from 'react-native';
 import funcaoEstilosPadrao from './estilos';
 
 export default function Botao({pequeno = false, invertido = false, valor, acao, estilos}) {
-    const estilosPadrao = funcaoEstilosPadrao(pequeno, invertido);
-    return <TouchableOpacity onPress={acao} style={[estilosPadrao.botao, estilos]}>
-        <Text style={estilosPadrao.valor}>{valor}</Text>
-    </TouchableOpacity>
+  const estilosPadrao = funcaoEstilosPadrao(pequeno, invertido);
+  return <TouchableOpacity onPress={acao} style={[estilosPadrao.botao, estilos]}>
+    <Text style={estilosPadrao.valor}>{valor}</Text>
+  </TouchableOpacity>
 }
--- src/componentes/CampoInteiro/index.js
+++ src/componentes/CampoInteiro/index.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import { TextInput } from 'react-native';
 
-import estilos from '../../estilos';
+import { TextInput } from 'react-native';
 import estilosPadrao from './estilos';
 
-export default function CampoInteiro({valor, acao}) {
-    const atualiza = (novoValor, acaoRetorno) => {
-        const verificaInteiro = novoValor.match(/^[0-9]*$/);
-        if (!verificaInteiro) return;
+export default function CampoInteiro({valor, estilos, acao}) {
+  const atualiza = (novoValor, acaoRetorno) => {
+    const verificaInteiro = novoValor.match(/^[0-9]*$/);
+    if (!verificaInteiro) return;
 
-        const removeZeroEsquerda = novoValor.replace(/^(0)(.*)/, '$2');
+    const removeZeroEsquerda = novoValor.replace(/^(0)(.+)/, '$2');
 
-        acaoRetorno(removeZeroEsquerda);
-    }
+    acaoRetorno(removeZeroEsquerda);
+  }
 
-    
-    const numeroEmTexto = String(valor);
-    
-    return <TextInput
-        style={[estilosPadrao.campo, estilos]}
-        keyboardType='number-pad'
-        selectTextOnFocus
-        onChangeText={(novoValor) => {atualiza(novoValor, acao)}}
-        value={numeroEmTexto}
-    />
+  const numeroEmTexto = String(valor);
+
+  return <TextInput 
+    style={[estilosPadrao.campo, estilos]}
+    keyboardType='number-pad'
+    selectTextOnFocus
+    onChangeText={(novoValor) => {atualiza(novoValor, acao)}}
+    value={numeroEmTexto}
+  />
 }
